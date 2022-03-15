db08afdb81671ee98cd320c544ba299fecc52cb3
Mikkel Ricky <rimi@aarhus.dk>
2022-03-15 12:48:15 +0100

    HRIN-677: Generated overview of hearings

--- src/Service/DeskproService.php
+++ src/Service/DeskproService.php
@@ -37,6 +37,72 @@ class DeskproService
         return $response->getData();
     }
 
+    private static $customFields = [
+        'hearing_id' => '28',
+        'hearing_name' => '30',
+        'edoc_id' => '15',
+        'pdf_download_url' => '22',
+        'representation' => '2',
+        'organization' => '7',
+        'address_secret' => '32',
+        'address' => '1',
+        'postal_code' => '37',
+        'geolocation' => '31',
+        'message' => '35',
+        'files' => '36',
+        'accept_terms' => '11',
+        'unpublish_reply' => '18',
+        'resume' => '33',
+        'vurdering' => '34',
+        'udtaler' => '2',
+    ];
+
+    public function getHearingTickets(int $hearingId): array
+    {
+        $tickets = [];
+
+        $currentPage = 1;
+        $totalPages = 1;
+
+        $query = [
+            'count' => 200,
+            'ticket_field.'.self::$customFields['hearing_id'] => $hearingId,
+            'include' => 'person',
+        ];
+        while ($currentPage <= $totalPages) {
+            $query['page'] = $currentPage;
+            $endpoint = '/tickets?' . http_build_query($query);
+            $response = $this->client()->get($endpoint);
+            $meta = $response->getMeta();
+            if ($meta['pagination']['total'] > 999) {
+                throw new \RuntimeException('Cannot reliably handle more than 999 tickets due to limitation in the Deskpro API');
+            }
+
+            $totalPages = $meta['pagination']['total_pages'];
+            $currentPage += 1;
+
+            $linked = $response->getLinked();
+            foreach ($response->getData() as $item) {
+                // Expand person in ticket from linked data.
+                if (isset($item['person'], $linked['person'], $linked['person'][$item['person']])) {
+                    $item['person'] = $linked['person'][$item['person']];
+                }
+
+                // Add readable names to field values
+                foreach (self::$customFields as $name => $id) {
+                    $item['fields'][$name] = $item['fields'][$id]['detail']
+                                             ?? $item['fields'][$name] = $item['fields'][$id]['detail']
+                                             ?? $item['fields'][$name] = $item['fields'][$id]['value']
+                                             ?? null;
+                }
+
+                $tickets[] = $item;
+            }
+        }
+
+        return $tickets;
+    }
+
     /**
      * Get a Deskpro client.
      */
